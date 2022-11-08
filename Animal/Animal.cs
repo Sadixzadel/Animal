19760f66d729f98b39838f9ca3d13e2c56878ef2
HP <HP@DESKTOP-EQ9MNTC>
2022-11-08 09:04:28 +0400

    Добавьте файлы проекта.

---
+++ Animal/Animal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal1;
+
+internal abstract class Animal
+{
+    private string Food;
+    private string Location;
+    
+    public Animal(string food, string location)
+    {
+        Food = food;
+        Location = location;
+    }
+
+    public abstract string Eat();
+    public abstract string MakeNoise();
+
+    //?
+    public string? Sleep()
+    {
+        return base.ToString();
+    }
+    
+    public class Dog : Animal
+    {
+        public string Name { get; set; }
+        public Dog(string food, string location, string name) : base(food, location)
+        {
+            Name = name;
+        }
+
+        public override string Eat()
+        {
+            return ("Dog is eating");
+        }
+
+        public override string MakeNoise()
+        {
+            return ("Gaf- gaf- gah");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Name { get; set; }
+        public Cat(string food, string location, string name) : base(food, location)
+        {
+            Name = name;
+        }
+
+
+        public override string Eat()
+        {
+            return ("Cat is eating");
+        }
+
+        public override string MakeNoise()
+        {
+            return ("Miu- miu- miu");
+        }
+    }
+
+    public class Horse : Animal
+    {
+        public string Name { get; set; }
+        public Horse(string food, string location, string name) : base(food, location)
+        {
+            Name = name;
+        }
+
+        public override string Eat()
+        {
+            return ("Horse is eating");
+        }
+
+        public override string MakeNoise()
+        {
+            return ("Ii- go- go");
+        }
+    }
+    
+    public class Veterinarian
+    {
+        public static string TreatAnimal(Animal animal)
+        {
+            return animal.ToString();
+        }
+    }
+
+}
---
+++ Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿using Animal1;
+using static Animal1.Animal;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog( "Bones", "Yard", "Rex");
+        animals[1] = new Cat("Fish", "Flat", "Kity");
+        animals[2] = new Horse( "Hay", "Horse farm", "Dora");
+
+        for (int i = 0; i < animals.Length; i++)
+        {
+            
+            Veterinarian.TreatAnimal(animal);
+        }
+    }
+}
